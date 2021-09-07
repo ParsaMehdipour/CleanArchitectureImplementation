6bc24b3c2d9ab6529c72bd36b593a4214db2a1e9
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-08 01:55:21 +0430

    feat : add users controller

---
+++ CleanArchitecture/EndPoint.Site/Areas/AdminPanel/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Areas.AdminPanel.Controllers
+{
+    public class UsersController : Controller
+    {
+        [Area("AdminPanel")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
