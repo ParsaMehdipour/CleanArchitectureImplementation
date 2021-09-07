2fa46dc849cd8f68585b63221340df1ad75d1698
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-08 01:56:23 +0430

    refactor : config area endpoint

--- CleanArchitecture/EndPoint.Site/Startup.cs
+++ CleanArchitecture/EndPoint.Site/Startup.cs
@@ -51,6 +51,11 @@ namespace EndPoint.Site
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
