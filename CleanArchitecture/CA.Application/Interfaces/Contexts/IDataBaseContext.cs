9113d64a5df9b22f0790e038daf99ae58dcef04b
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-09 10:29:30 +0430

    feat : add i data base context interface

---
+++ CleanArchitecture/CA.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CA.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Role> Roles { get; set; }
+        DbSet<UserRole> UserRoles { get; set; }
+
+
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    }
+}
