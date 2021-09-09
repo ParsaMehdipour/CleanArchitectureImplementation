3f5cb23653c6937d9e718368abeeeb1d0c6de058
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-09 10:30:28 +0430

    feat : add user role domain class

---
+++ CleanArchitecture/CA.Domain/Entities/Users/UserRole.cs
@@ -0,0 +1,13 @@
+﻿namespace CA.Domain.Entities.Users
+{
+    public class UserRole
+    {
+        public long Id { get; set; }
+
+        public long UserId { get; set; }
+        public User User { get; set; }
+
+        public long RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
