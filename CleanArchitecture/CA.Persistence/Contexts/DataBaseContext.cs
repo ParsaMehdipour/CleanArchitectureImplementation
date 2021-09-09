e480c9747b64a53ecb9ca90b1fac66d996b96011
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-09 10:29:13 +0430

    feat : add data base context class

---
+++ CleanArchitecture/CA.Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using CA.Application.Interfaces.Contexts;
+using CA.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Persistence.Contexts
+{
+    public class DataBaseContext : DbContext , IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) 
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
