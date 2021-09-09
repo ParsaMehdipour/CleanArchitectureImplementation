e1c13fd7f6bdaccc5c3d7d6a1f1bcdf97ff1a556
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-09 10:30:46 +0430

    feat : add persistence bootstrapper class

---
+++ CleanArchitecture/CA.Persistence/Bootstrapper/PersistenceBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using CA.Application.Interfaces.Contexts;
+using CA.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CA.Persistence.Bootstrapper
+{
+    public class PersistenceBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionstring)
+        {
+            services.AddDbContext<DataBaseContext>(options =>
+            {
+                options.UseSqlServer(connectionstring);
+            });
+
+
+            services.AddTransient<IDataBaseContext, DataBaseContext>();
+        }
+    }
+}
