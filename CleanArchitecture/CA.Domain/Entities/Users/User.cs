a7290a8b3113231368afbff026f9e2d6e40a7f8c
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-09 10:30:18 +0430

    feat : add user domain class

---
+++ CleanArchitecture/CA.Domain/Entities/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Domain.Entities.Users
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public List<UserRole> UserRoles { get; set; }
+    }
+}
