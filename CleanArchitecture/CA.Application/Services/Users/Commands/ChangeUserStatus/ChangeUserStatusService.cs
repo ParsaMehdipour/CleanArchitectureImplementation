45a4ba635ab49db08323a6be82211126a3e7316f
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:42:39 +0430

    feat : add change user status service class

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/ChangeUserStatus/ChangeUserStatusService.cs
@@ -0,0 +1,38 @@
+﻿using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.ChangeUserStatus
+{
+    public class ChangeUserStatusService : IChangeUserStatusService
+    {
+        private readonly IDataBaseContext _context;
+
+
+        public ChangeUserStatusService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto Execute(long UserId)
+        {
+            var user = _context.Users.Find( UserId);
+            if (user == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "کاربر یافت نشد"
+                };
+            }
+
+            user.IsActive = !user.IsActive;
+            _context.SaveChanges();
+            string userstate = user.IsActive == true ? "فعال" : "غیر فعال";
+            return new ResultDto()
+            {
+                IsSuccess = true,
+                Message = $"کاربر با موفقیت {userstate} شد!",
+            };
+        }
+    }
+}
