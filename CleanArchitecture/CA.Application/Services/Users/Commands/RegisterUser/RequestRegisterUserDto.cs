0147e53eeafe35efd76399b9771ed3ea65a160dd
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:25:55 +0430

    feat : add request register user

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/RegisterUser/RequestRegisterUserDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Users.Commands.RegisterUser
+{
+    public class RequestRegisterUserDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public List<RoleInRegisterUserDto> Roles { get; set; }
+    }
+}
