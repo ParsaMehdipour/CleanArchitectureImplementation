9e8d21bcb49a5691b340b5ddd64d944757435afe
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:46:26 +0430

    refactor : inheriet from base entity and add is active property

--- CleanArchitecture/CA.Domain/Entities/Users/User.cs
+++ CleanArchitecture/CA.Domain/Entities/Users/User.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using CA.Domain.Entities.Common;
 
 namespace CA.Domain.Entities.Users
 {
-    public class User
+    public class User : BaseEntity
     {
-        public long Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public bool IsActive { get; set; }
 
         public List<UserRole> UserRoles { get; set; }
     }
