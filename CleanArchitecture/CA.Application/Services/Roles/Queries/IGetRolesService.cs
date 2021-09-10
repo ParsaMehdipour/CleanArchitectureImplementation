9cb05e5e8fc18ac4b895a0a8e40281e419706c32
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:24:15 +0430

    feat : add i get roles service inreface

---
+++ CleanArchitecture/CA.Application/Services/Roles/Queries/IGetRolesService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Roles.Queries
+{
+    public interface IGetRolesService
+    {
+        ResultDto<List<RolesDto>> Execute();
+    }
+}
