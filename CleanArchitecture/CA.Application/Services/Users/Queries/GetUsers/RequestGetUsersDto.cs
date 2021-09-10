9e6d19df9bbe03b10a26f03db69e51fda0a2e461
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:23:14 +0430

    feat : add reaquest get users dto

---
+++ CleanArchitecture/CA.Application/Services/Users/Queries/GetUsers/RequestGetUsersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CA.Application.Services.Users.Queries.GetUsers
+{
+    public class RequestGetUsersDto
+    {
+        public string SearchKey { get; set; }
+        public int Page { get; set; } = 1;
+    }
+}
