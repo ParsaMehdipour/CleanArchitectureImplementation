d6af264135bd981ccfc46b008e6fa5055a1f992e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 00:38:13 +0430

    feat : add result login user dto

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/LoginUser/ResultLoginUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CA.Application.Services.Users.Commands.LoginUser
+{
+    public class ResultLoginUserDto
+    {
+        public long UserId { get; set; }
+        public string Roles { get; set; }
+        public string Name { get; set; }
+    }
+}
