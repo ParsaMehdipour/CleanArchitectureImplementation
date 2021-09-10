8691aabfe10489959fa1dd769294160b26630c71
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:27:42 +0430

    feat : add result dto

---
+++ CleanArchitecture/CA.Common/Dto/ResultDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CA.Common.Dto
+{
+    public class ResultDto
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ResultDto<TData>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public TData Data { get; set; }
+    }
+}
