6d2eebf40b5e8b1b774d30bf0be6556cc529561e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:29:16 +0430

    refactor : seed roles to data base

--- CleanArchitecture/CA.Persistence/Contexts/DataBaseContext.cs
+++ CleanArchitecture/CA.Persistence/Contexts/DataBaseContext.cs
@@ -1,19 +1,30 @@
 ﻿using CA.Application.Interfaces.Contexts;
+using CA.Common.Roles;
 using CA.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace CA.Persistence.Contexts
 {
-    public class DataBaseContext : DbContext , IDataBaseContext
+    public class DataBaseContext : DbContext, IDataBaseContext
     {
-        public DataBaseContext(DbContextOptions<DataBaseContext> options) 
+        public DataBaseContext(DbContextOptions<DataBaseContext> options)
             : base(options)
         {
-            
+
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Role>().HasData(new Role { Id = 1, Name = nameof(UserRolesNames.Admin) });
+            modelBuilder.Entity<Role>().HasData(new Role { Id = 2, Name = nameof(UserRolesNames.Operator) });
+            modelBuilder.Entity<Role>().HasData(new Role { Id = 3, Name = nameof(UserRolesNames.Customer) });
+
+        }
     }
 }
