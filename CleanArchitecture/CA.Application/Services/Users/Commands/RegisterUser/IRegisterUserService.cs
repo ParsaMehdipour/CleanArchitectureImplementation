bc9f19387185e08d81bf434e284c7034d884f54c
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:24:48 +0430

    feat : add i register user service

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/RegisterUser/IRegisterUserService.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.RegisterUser
+{
+    public interface IRegisterUserService
+    {
+        ResultDto<ResultRegisterUserDto> Execute(RequestRegisterUserDto request);
+    }
+}
