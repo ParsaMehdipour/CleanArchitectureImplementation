1ef47e6108b18014b04b23e19d8ccbd14bf51080
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:24:30 +0430

    feat : add get roles service

---
+++ CleanArchitecture/CA.Application/Services/Roles/Queries/GetRolesService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Roles.Queries
+{
+    public class GetRolesService : IGetRolesService
+    {
+        private readonly IDataBaseContext _context;
+
+        public GetRolesService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public ResultDto<List<RolesDto>> Execute()
+        {
+            var roles = _context.Roles.ToList().Select(p => new RolesDto
+            {
+                Id = p.Id,
+                Name = p.Name
+            }).ToList();
+
+            return new ResultDto<List<RolesDto>>()
+            {
+                Data = roles,
+                IsSuccess = true,
+                Message = "",
+            };
+        }
+    }
+}
