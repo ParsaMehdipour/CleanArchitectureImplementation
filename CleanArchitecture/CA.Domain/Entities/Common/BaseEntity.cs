c4ea2e99a2f9f18776280363b47ba5243a29ecf5
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:40:29 +0430

    feat : add base entity

---
+++ CleanArchitecture/CA.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CA.Domain.Entities.Common
+{
+
+    public abstract class BaseEntity<TKey>  
+    {
+        public TKey Id { get; set; }
+        public DateTime InsertTime { get; set; } = DateTime.Now;
+        public DateTime? UpdateTime { get; set; }
+        public bool IsRemoved { get; set; } = false;
+        public DateTime? RemoveTime { get; set; }
+    }
+    public abstract class BaseEntity : BaseEntity<long>
+    {
+    }
+}
