e29287b04b67f2c6e46543b8952f939c80464de9
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:28:20 +0430

    feat : add pagination class

---
+++ CleanArchitecture/CA.Common/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CA.Common
+{
+    public static class Pagination
+    {
+        public static IEnumerable<TSource> ToPaged<TSource>(this IEnumerable<TSource> source, int page, int pageSize, out int rowsCount)
+        {
+            rowsCount = source.Count();
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
