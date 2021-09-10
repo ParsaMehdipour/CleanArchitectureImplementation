b91876da068fcc62b341dfcc6521f57443bf1323
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 01:16:19 +0430

    feat : add sign up view model class

---
+++ CleanArchitecture/EndPoint.Site/Models/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EndPoint.Site.Models
+{
+    public class SignUpViewModel
+    {
+        public string FullName { get; set; } = "";
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+    }
+}
