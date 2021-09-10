6f09bbfaa11d62acb990a3b523dd803df5c3ccb7
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:23:30 +0430

    feat : add result get users dto

---
+++ CleanArchitecture/CA.Application/Services/Users/Queries/GetUsers/ResultGetUsersDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CA.Application.Services.Users.Queries.GetUsers
+{
+    public class ResultGetUsersDto
+    {
+        public List<GetUserDto> GetUserDtos { get; set; }
+        public int Rows { get; set; }
+    }
+}
