b52d957723642bd5709452fcc1798a033f761776
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:40:40 +0430

    feat : add base entity not id

---
+++ CleanArchitecture/CA.Domain/Entities/Common/BaseEntityNotId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CA.Domain.Entities.Common
+{
+    public abstract class BaseEntityNotId  
+    {
+        public DateTime InsertTime { get; set; } = DateTime.Now;
+        public DateTime? UpdateTime { get; set; }
+        public bool IsRemoved { get; set; } = false;
+        public DateTime? RemoveTime { get; set; }
+    }
+}
