c8c4ac085f5e7fc3953cdb0f6f40d294f0a135b7
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 00:37:59 +0430

    feat : add i login user service interface

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/LoginUser/ILoginUserService.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.LoginUser
+{
+    public interface ILoginUserService
+    {
+        ResultDto<ResultUserloginDto> Execute(string Username, string Password);
+    }
+}
