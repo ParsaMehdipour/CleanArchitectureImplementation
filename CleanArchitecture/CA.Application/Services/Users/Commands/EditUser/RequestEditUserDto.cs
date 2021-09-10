83d4c40c3d26334db69d3632a6567ca435616179
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:43:29 +0430

    feat : add request edit user dto class

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/EditUser/RequestEditUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CA.Application.Services.Users.Commands.EditUser
+{
+    public class RequestEditUserDto
+    {
+        public long UserId { get; set; }
+        public string Fullname { get; set; }
+    }
+}
