c8df72081f3377a97916944e3c7406fff424c237
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:43:11 +0430

    feat : add edit user servie class

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/EditUser/EditUserService.cs
@@ -0,0 +1,37 @@
+﻿using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.EditUser
+{
+    public class EditUserService : IEditUserService
+    {
+        private readonly IDataBaseContext _context;
+
+        public EditUserService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public ResultDto Execute(RequestEditUserDto request)
+        {
+            var user = _context.Users.Find(request.UserId);
+            if (user == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "کاربر یافت نشد"
+                };
+            }
+
+            user.Name = request.Fullname;
+            _context.SaveChanges();
+
+            return new ResultDto()
+            {
+                IsSuccess = true,
+                Message = "ویرایش کاربر انجام شد"
+            };
+
+        }
+    }
+}
