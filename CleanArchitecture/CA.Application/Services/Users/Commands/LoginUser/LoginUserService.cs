130c6f8e9b4d0bb18eea708c7c7172b07fac13dd
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 00:37:45 +0430

    feat : add login user service class

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/LoginUser/LoginUserService.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using CA.Application.Interfaces.Contexts;
+using CA.Common;
+using CA.Common.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Services.Users.Commands.LoginUser
+{
+    public class LoginUserService : ILoginUserService
+    {
+        private readonly IDataBaseContext _context;
+        public LoginUserService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public ResultDto<ResultUserloginDto> Execute(string Username, string Password)
+        {
+
+            if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrWhiteSpace(Password))
+            {
+                return new ResultDto<ResultUserloginDto>()
+                {
+                    Data = new ResultUserloginDto()
+                    {
+
+                    },
+                    IsSuccess = false,
+                    Message = "نام کاربری و رمز عبور را وارد نمایید",
+                };
+            }
+
+
+
+            var user = _context.Users
+                .Include(p => p.UserRoles)
+                .ThenInclude(p => p.Role)
+                .FirstOrDefault(p => p.Email.Equals(Username)
+                                     && p.IsActive == true);
+
+            if (user == null)
+            {
+                return new ResultDto<ResultUserloginDto>()
+                {
+                    Data = new ResultUserloginDto()
+                    {
+
+                    },
+                    IsSuccess = false,
+                    Message = "کاربری با این ایمیل در سایت فروشگاه ثبت نام نکرده است",
+                };
+            }
+
+            var passwordHasher = new PasswordHasher();
+            bool resultVerifyPassword = passwordHasher.VerifyPassword(user.Password, Password);
+            if (resultVerifyPassword == false)
+            {
+                return new ResultDto<ResultUserloginDto>()
+                {
+                    Data = new ResultUserloginDto()
+                    {
+
+                    },
+                    IsSuccess = false,
+                    Message = "رمز وارد شده اشتباه است!",
+                };
+            }
+
+
+            var roles = "";
+            foreach (var item in user.UserRoles)
+            {
+                roles += $"{item.Role.Name}";
+            }
+
+
+            return new ResultDto<ResultUserloginDto>()
+            {
+                Data = new ResultUserloginDto()
+                {
+                    Roles = roles,
+                    UserId = user.Id,
+                    Name = user.Name
+                },
+                IsSuccess = true,
+                Message = "ورود به سایت با موفقیت انجام شد",
+            };
+
+
+        }
+    }
+}
