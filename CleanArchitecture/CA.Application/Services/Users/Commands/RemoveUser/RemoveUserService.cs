27dee66df08ab71883b33963cbe8280d8e9bdfc0
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:45:04 +0430

    feat : add remove user servie class

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/RemoveUser/RemoveUserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.RemoveUser
+{
+    public class RemoveUserService : IRemoveUserService
+    {
+        private readonly IDataBaseContext _context;
+
+        public RemoveUserService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto Execute(long userId)
+        {
+
+            var user = _context.Users.Find(userId);
+            if (user == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "کاربر یافت نشد"
+                };
+            }
+            user.RemoveTime = DateTime.Now;
+            user.IsRemoved = true;
+            _context.SaveChanges();
+            return new ResultDto()
+            {
+                IsSuccess = true,
+                Message = "کاربر با موفقیت حذف شد"
+            };
+        }
+    }
+}
