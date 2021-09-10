0e100b333c0fb1632a611c84504e32ac9142ea08
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:44:31 +0430

    refactor : add password and repassword properties

--- CleanArchitecture/CA.Application/Services/Users/Commands/RegisterUser/RequestRegisterUserDto.cs
+++ CleanArchitecture/CA.Application/Services/Users/Commands/RegisterUser/RequestRegisterUserDto.cs
@@ -6,6 +6,9 @@ namespace CA.Application.Services.Users.Commands.RegisterUser
     {
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+
         public List<RoleInRegisterUserDto> Roles { get; set; }
     }
 }
