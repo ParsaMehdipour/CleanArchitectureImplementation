01e72dc5b0ae54774e8a1a7d515d64f4f1360ce2
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 14:30:44 +0430

    fix : fix bugs

--- CleanArchitecture/EndPoint.Site/Areas/AdminPanel/Controllers/UsersController.cs
+++ CleanArchitecture/EndPoint.Site/Areas/AdminPanel/Controllers/UsersController.cs
@@ -3,14 +3,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CA.Application.Services.Roles.Queries;
+using CA.Application.Services.Users.Queries.GetUsers;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EndPoint.Site.Areas.AdminPanel.Controllers
 {
+    [Area("AdminPanel")]
     public class UsersController : Controller
     {
-        [Area("AdminPanel")]
-        public IActionResult Index()
+        private readonly IGetUsersService _getUsersService;
+        private readonly IGetRolesService _getRolesService;
+
+        public UsersController(IGetUsersService getUsersService,IGetRolesService getRolesService)
         {
+            _getUsersService = getUsersService;
+            _getRolesService = getRolesService;
+        }
+
+        public IActionResult Index(RequestGetUsersDto request)
+        {
+            var model = _getUsersService.Execute(request);
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Roles = new SelectList(_getRolesService.Execute().Data, "Id", "Name");
             return View();
         }
     }
