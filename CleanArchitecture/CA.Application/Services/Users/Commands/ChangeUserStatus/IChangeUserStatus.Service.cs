4183a763a38ba3ff011d598ec297b19bcc30f256
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:42:22 +0430

    feat : add i change user status service interface

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/ChangeUserStatus/IChangeUserStatus.Service.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.ChangeUserStatus
+{
+    public interface IChangeUserStatusService
+    {
+        ResultDto Execute(long UserId);
+    }
+}
