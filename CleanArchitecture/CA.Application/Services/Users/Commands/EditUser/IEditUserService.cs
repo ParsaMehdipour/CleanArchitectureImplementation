00fa73b249472a183f39326403eb992b100a9bea
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:42:58 +0430

    feat : add i edit user servie interface

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/EditUser/IEditUserService.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.EditUser
+{
+    public interface IEditUserService
+    {
+        ResultDto Execute(RequestEditUserDto request);
+    }
+}
