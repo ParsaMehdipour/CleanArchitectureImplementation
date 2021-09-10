7621e5460b66db5e878dc29b5782f52749355cd9
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-10 17:44:50 +0430

    feat : add i remove user serivce interface

---
+++ CleanArchitecture/CA.Application/Services/Users/Commands/RemoveUser/IRemoveUserService.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Users.Commands.RemoveUser
+{
+    public interface IRemoveUserService
+    {
+        ResultDto Execute(long userId);
+    }
+}
