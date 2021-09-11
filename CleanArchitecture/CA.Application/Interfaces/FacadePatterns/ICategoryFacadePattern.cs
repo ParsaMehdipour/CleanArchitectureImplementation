d8adaf07db5dfc334cbdc25bc8c0c8fe5e2e0481
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:15:29 +0430

    feat : add i category facade pattern interface

---
+++ CleanArchitecture/CA.Application/Interfaces/FacadePatterns/ICategoryFacadePattern.cs
@@ -0,0 +1,11 @@
+﻿using CA.Application.Services.Category.Commands.AddNewCategory;
+using CA.Application.Services.Category.Queries.GetCategories;
+
+namespace CA.Application.Interfaces.FacadePatterns
+{
+    public interface ICategoryFacadePattern
+    {
+        public AddNewCategoryService AddNewCategoryService { get; }
+        public IGetCategoriesService GetCategoriesService { get; }
+    }
+}
