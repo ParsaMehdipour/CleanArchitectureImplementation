0a6491ff7f0ffeb9a1a5fdedf2a06bc9bd244c28
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:13:47 +0430

    feat : add category domain class

---
+++ CleanArchitecture/CA.Domain/Entities/Category/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using CA.Domain.Entities.Common;
+
+namespace CA.Domain.Entities.Category
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public virtual Category ParentCategory { get; set; }
+        public long? ParentCategoryId { get; set; }
+
+
+        //برای نمایش زیر دسته های هر گروه
+        public virtual ICollection<Category> SubCategories { get; set; }
+
+    }
+}
