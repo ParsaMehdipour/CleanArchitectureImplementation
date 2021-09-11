2aab70cdc35d9919af3ea184eeeffde016527917
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:16:06 +0430

    feat : i add new category service interface added

---
+++ CleanArchitecture/CA.Application/Services/Category/Commands/AddNewCategory/IAddNewCategory.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Category.Commands.AddNewCategory
+{
+    public interface IAddNewCategoryService
+    {
+        ResultDto Execute(long? ParentId, string Name);
+    }
+}
