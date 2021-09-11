14d4d728363fc0caa27bddce116b424a8dc65243
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:16:43 +0430

    feat : add get categories service class

---
+++ CleanArchitecture/CA.Application/Services/Category/Queries/GetCategories/GetCategoriesService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Services.Category.Queries.GetCategories
+{
+    public class GetCategoriesService : IGetCategoriesService
+    {
+        private readonly IDataBaseContext _context;
+
+        public GetCategoriesService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<List<CategoriesDto>> Execute(long? ParentId)
+        {
+            var categories = _context.Categories
+                .Include(p => p.ParentCategory)
+                .Include(p => p.SubCategories)
+                .Where(p => p.ParentCategoryId == ParentId)
+                .ToList()
+                .Select(p => new CategoriesDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Parent = p.ParentCategory != null ? new
+                            ParentCategoryDto
+                            {
+                                Id = p.ParentCategory.Id,
+                                name = p.ParentCategory.Name,
+                            }
+                        : null,
+                    HasChild = p.SubCategories.Count() > 0 ? true : false,
+                }).ToList();
+
+
+            return new ResultDto<List<CategoriesDto>>()
+            {
+                Data = categories,
+                IsSuccess = true,
+                Message = "لیست باموقیت برگشت داده شد"
+            };
+
+        }
+    }
+}
