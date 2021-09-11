b056006c595a527f4102aa4c87d990bdbad320f9
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:16:19 +0430

    feat : add category facade pattern class

---
+++ CleanArchitecture/CA.Application/Services/Category/FacadePattern/CategoryFacadePattern.cs
@@ -0,0 +1,37 @@
+﻿using CA.Application.Interfaces.Contexts;
+using CA.Application.Interfaces.FacadePatterns;
+using CA.Application.Services.Category.Commands.AddNewCategory;
+using CA.Application.Services.Category.Queries.GetCategories;
+
+namespace CA.Application.Services.Category.FacadePattern
+{
+    public class CategoryFacadePattern : ICategoryFacadePattern
+    {
+        private readonly IDataBaseContext _context;
+
+        public CategoryFacadePattern(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        private AddNewCategoryService _addNewCategoryService;
+
+        public AddNewCategoryService AddNewCategoryService
+        {
+            get
+            {
+                return _addNewCategoryService ??= new AddNewCategoryService(_context);
+            }
+        }
+
+        private IGetCategoriesService _getCategoriesService;
+
+        public IGetCategoriesService GetCategoriesService
+        {
+            get
+            {
+                return _getCategoriesService ??= new GetCategoriesService(_context);
+            }
+        }
+    }
+}
