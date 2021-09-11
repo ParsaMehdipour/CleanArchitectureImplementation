2d74defd0491929060e8f0e121d22a63af09b872
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:18:16 +0430

    refctor : add categories dbset and refactor to a better code

--- CleanArchitecture/CA.Persistence/Contexts/DataBaseContext.cs
+++ CleanArchitecture/CA.Persistence/Contexts/DataBaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using CA.Application.Interfaces.Contexts;
 using CA.Common.Roles;
+using CA.Domain.Entities.Category;
 using CA.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,19 +17,32 @@ namespace CA.Persistence.Contexts
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
-            modelBuilder.Entity<Role>().HasData(new Role { Id = 1, Name = nameof(UserRolesNames.Admin) });
-            modelBuilder.Entity<Role>().HasData(new Role { Id = 2, Name = nameof(UserRolesNames.Operator) });
-            modelBuilder.Entity<Role>().HasData(new Role { Id = 3, Name = nameof(UserRolesNames.Customer) });
-
+            SeedData(modelBuilder);
 
             modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique();
 
-            modelBuilder.Entity<User>().HasQueryFilter(x => !x.IsRemoved);
+            ApplyQueryFilter(modelBuilder);
         }
+
+        private void ApplyQueryFilter(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasQueryFilter(p => !p.IsRemoved);
+            modelBuilder.Entity<Role>().HasQueryFilter(p => !p.IsRemoved);
+            modelBuilder.Entity<UserRole>().HasQueryFilter(p => !p.IsRemoved);
+            modelBuilder.Entity<Category>().HasQueryFilter(p => !p.IsRemoved);
+        }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(new Role { Id = 1, Name = nameof(UserRolesNames.Admin) });
+            modelBuilder.Entity<Role>().HasData(new Role { Id = 2, Name = nameof(UserRolesNames.Operator) });
+            modelBuilder.Entity<Role>().HasData(new Role { Id = 3, Name = nameof(UserRolesNames.Customer) });
+        }
+
     }
 }
