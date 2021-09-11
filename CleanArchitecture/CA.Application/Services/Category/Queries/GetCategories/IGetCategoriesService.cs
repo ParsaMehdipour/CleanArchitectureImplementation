823b9cf78ac4028859d6a401790e25a186a60e4c
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:16:58 +0430

    feat : add i get categories service interface

---
+++ CleanArchitecture/CA.Application/Services/Category/Queries/GetCategories/IGetCategoriesService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Category.Queries.GetCategories
+{
+    public interface IGetCategoriesService
+    {
+        ResultDto<List<CategoriesDto>> Execute(long? ParentId);
+    }
+}
+
+
+
