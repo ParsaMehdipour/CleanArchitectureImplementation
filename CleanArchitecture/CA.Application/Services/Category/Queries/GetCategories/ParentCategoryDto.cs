a57bfeaa1fd390d81c29fda424dbb3a5ab350321
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-11 17:17:10 +0430

    feat : add parent category dto

---
+++ CleanArchitecture/CA.Application/Services/Category/Queries/GetCategories/ParentCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CA.Application.Services.Category.Queries.GetCategories
+{
+    public class ParentCategoryDto
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+    }
+}
