75cbfc5c1c87547b2a615bc4686074d29faceedd
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:12:01 +0430

    feat : add get all categories service class

---
+++ CleanArchitecture/CA.Application/Services/Category/Queries/GetAllCategories/GetAllCategoriesService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Services.Category.Queries.GetAllCategories
+{
+    public class GetAllCategoriesService : IGetAllCategoriesService
+    {
+        private readonly IDataBaseContext _context;
+
+        public GetAllCategoriesService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<List<AllCategoriesDto>> Execute()
+        {
+            var categories = _context
+                .Categories
+                .Include(p => p.ParentCategory)
+                .Where(p => p.ParentCategoryId != null)
+                .ToList()
+                .Select(p => new AllCategoriesDto
+                    {
+                        Id = p.Id,
+                        Name = $"{p.ParentCategory.Name} - {p.Name}",
+                    }
+                ).ToList();
+
+            return new ResultDto<List<AllCategoriesDto>>
+            {
+                Data = categories,
+                IsSuccess = false,
+                Message = "",
+            };
+        }
+    }
+}
