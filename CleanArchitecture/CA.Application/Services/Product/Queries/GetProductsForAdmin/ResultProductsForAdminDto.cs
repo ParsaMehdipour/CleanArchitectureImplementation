f033e443c1a9fe98d2d7be5145affde00fc1ecc8
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:35:48 +0430

    feat : add result products for admin dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductsForAdmin/ResultProductsForAdminDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Product.Queries.GetProductsForAdmin
+{
+    public class ResultProductsForAdminDto
+    {
+        ///<summary>
+        /// For Pagination
+        /// </summary>
+        public int RowCount { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+
+        public List<ProductForAdminListDto> Products { get; set; }
+    }
+}
