364acccd7ebd396d93acaa7f6707f3011aa393ee
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:33:36 +0430

    feat : add i sget product details for admin service interface

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductDetailsForAdmin/IGetProductDetailsForAdminService.cs
@@ -0,0 +1,10 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Product.Queries.GetProductDetailsForAdmin
+{
+    public interface IGetProductDetailsForAdminService
+    {
+        ResultDto<ProductDetailsForAdminDto> Execute(long Id);
+
+    }
+}
