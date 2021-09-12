44d448a8b12d83486b2b5fed44774e8a5236bb73
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 23:31:44 +0430

    feat : add result product for site dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductsForSite/ResultProductsForSiteDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Product.Queries.GetProductsForSite
+{
+    public class ResultProductsForSiteDto
+    {
+
+        public List<ProductForSiteDto> Products { get; set; }
+        public int TotalRow { get; set; }
+    }
+}
