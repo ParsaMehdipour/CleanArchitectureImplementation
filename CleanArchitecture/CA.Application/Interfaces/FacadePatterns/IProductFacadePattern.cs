7bc190e1266ccb8f46549431174e3551b9760ab0
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:31:56 +0430

    refactor : add get products and product details for admin property

--- CleanArchitecture/CA.Application/Interfaces/FacadePatterns/IProductFacadePattern.cs
+++ CleanArchitecture/CA.Application/Interfaces/FacadePatterns/IProductFacadePattern.cs
@@ -1,10 +1,19 @@
 ﻿using CA.Application.Services.Category.Commands.AddNewCategory;
 using CA.Application.Services.Product.Commands.AddNewProduct;
+using CA.Application.Services.Product.Queries.GetProductDetailsForAdmin;
+using CA.Application.Services.Product.Queries.GetProductsForAdmin;
 
 namespace CA.Application.Interfaces.FacadePatterns
 {
     public interface IProductFacadePattern
     {
-        public AddNewProductService AddNewProductService { get;}
+        public AddNewProductService AddNewProductService { get; }
+
+        /// <summary>
+        /// دریافت لیست محصولات برای پنل مدیریت
+        /// </summary>
+        public IGetProductsForAdminService GetProductsForAdminService { get; }
+
+        public IGetProductDetailsForAdminService GetProductDetailsForAdminService { get; }
     }
 }
