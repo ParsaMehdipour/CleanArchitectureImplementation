b8ba1957f9fb0d396ec4ba3fab8f6c9fbc52338d
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:12:51 +0430

    feat : add new product features dto class added

---
+++ CleanArchitecture/CA.Application/Services/Product/Commands/AddNewProduct/AddNewProductFeatures.cs
@@ -0,0 +1,8 @@
+﻿namespace CA.Application.Services.Product.Commands.AddNewProduct
+{
+    public class AddNewProductFeaturesDto
+    {
+        public string DisplayName { get; set; }
+        public string Value { get; set; }
+    }
+}
