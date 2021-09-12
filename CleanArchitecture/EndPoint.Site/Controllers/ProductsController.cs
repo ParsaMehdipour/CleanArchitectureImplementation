803b8cc866da3f49446d47de0ac50398b6ce391e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 23:32:36 +0430

    feat : add products controller

---
+++ CleanArchitecture/EndPoint.Site/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using CA.Application.Interfaces.FacadePatterns;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductFacadePattern _productFacade;
+
+        public ProductsController(IProductFacadePattern productFacade)
+        {
+            _productFacade = productFacade;
+        }
+        public IActionResult Index(int page=1)
+        {
+            return View(_productFacade.GetProductsForSiteService.Execute(page).Data);
+        }
+
+
+        //public IActionResult Detail(long Id)
+        //{
+        //    //return View(_productFacade.GetProductsDetailForSiteService.Execute(Id).Data);
+        //}
+    }
+}
