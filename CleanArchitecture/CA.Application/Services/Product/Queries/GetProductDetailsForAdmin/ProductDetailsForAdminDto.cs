dd19019618e36751885b4cd455bfd86ba7d7169c
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:34:18 +0430

    feat : add product details for admin dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductDetailsForAdmin/ProductDetailsForAdminDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Product.Queries.GetProductDetailsForAdmin
+{
+    public class ProductDetailsForAdminDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Inventory { get; set; }
+        public bool Displayed { get; set; }
+        public List<ProductDetailsFeatureDto> Features { get; set; }
+        public List<ProductDetailsImagesDto> Images { get; set; }
+    }
+}
