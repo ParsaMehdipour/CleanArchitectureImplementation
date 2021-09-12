3bec87472bd670c47fbe87919910dbb498156fc2
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:22:15 +0430

    feat : add product domain class

---
+++ CleanArchitecture/CA.Domain/Entities/Product/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CA.Domain.Entities.Common;
+
+namespace CA.Domain.Entities.Product
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Inventory { get; set; }
+        public bool Displayed { get; set; } //To Be Displayed In Site
+
+        public virtual Category.Category Category { get; set; }//Each Product Has One Product Category
+        public long CategoryId { get; set; }
+
+        public virtual ICollection<ProductImages> ProductImages { get; set; }//Each Product Has Many Images
+        public virtual ICollection<ProductFeatures> ProductFeatures { get; set; }//Each Product Has Many Features
+    }
+}
