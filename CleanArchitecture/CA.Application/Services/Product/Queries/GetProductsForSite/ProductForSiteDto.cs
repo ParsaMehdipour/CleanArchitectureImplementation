e7e2257092ab29b5731470b3d62ee1987c5e7271
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 23:31:24 +0430

    feat : add product for site dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductsForSite/ProductForSiteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CA.Application.Services.Product.Queries.GetProductsForSite
+{
+    public class ProductForSiteDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string ImageSrc { get; set; }
+        public int Star { get; set; }
+        public int Price { get; set; }
+    }
+}
