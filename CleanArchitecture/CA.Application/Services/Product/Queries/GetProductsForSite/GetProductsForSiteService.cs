3e155844a2012af564fe1ff89fc30dd213535e5e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 23:30:23 +0430

    feat : add get products for site service class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductsForSite/GetProductsForSiteService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using CA.Application.Interfaces.Contexts;
+using CA.Common;
+using CA.Common.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Services.Product.Queries.GetProductsForSite
+{
+    public class GetProductsForSiteService : IGetProductsForSiteService
+    {
+
+        private readonly IDataBaseContext _context;
+        public GetProductsForSiteService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+        public ResultDto<ResultProductsForSiteDto> Execute(int Page)
+        {
+            var products = _context.Products
+                .Include(p=>p.ProductImages)
+                .ToPaged(Page, 5, out var totalRow);
+
+            Random rd = new Random();
+            return new ResultDto<ResultProductsForSiteDto>
+            {
+                Data = new ResultProductsForSiteDto
+                {
+                    TotalRow = totalRow,
+                    Products = products.Select(p => new ProductForSiteDto
+                    {
+                        Id = p.Id,
+                        Star = rd.Next(1, 5),
+                        Title = p.Name,
+                        ImageSrc = p.ProductImages.FirstOrDefault().Src,
+                        Price=p.Price
+                    }).ToList(),
+                },
+                IsSuccess = true,
+            };
+        }
+    }
+}
