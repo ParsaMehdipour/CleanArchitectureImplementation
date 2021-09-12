986706ef4601ad6b3bdb135c3daeee889833cba7
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:22:48 +0430

    feat : add product images domain class

---
+++ CleanArchitecture/CA.Domain/Entities/Product/ProductImages.cs
@@ -0,0 +1,11 @@
+﻿using CA.Domain.Entities.Common;
+
+namespace CA.Domain.Entities.Product
+{
+    public class ProductImages : BaseEntity
+    {
+        public virtual Product Product { get; set; }
+        public long ProductId { get; set; }
+        public string Src { get; set; }
+    }
+}
