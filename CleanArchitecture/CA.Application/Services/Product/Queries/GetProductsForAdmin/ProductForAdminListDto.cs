94c7e7720d563a0173216118131ac182e6ee291e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:35:33 +0430

    feat : add product for admin list dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductsForAdmin/ProductForAdminListDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CA.Application.Services.Product.Queries.GetProductsForAdmin
+{
+    public class ProductForAdminListDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Inventory { get; set; }
+        public bool Displayed { get; set; }
+    }
+}
