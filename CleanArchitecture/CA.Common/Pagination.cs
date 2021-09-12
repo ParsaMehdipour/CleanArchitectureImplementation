e89a7bab207e6b38bbc614cd0a229403307997d0
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:36:54 +0430

    refactor : change to i queryable

--- CleanArchitecture/CA.Common/Pagination.cs
+++ CleanArchitecture/CA.Common/Pagination.cs
@@ -5,7 +5,7 @@ namespace CA.Common
 {
     public static class Pagination
     {
-        public static IEnumerable<TSource> ToPaged<TSource>(this IEnumerable<TSource> source, int page, int pageSize, out int rowsCount)
+        public static IQueryable<TSource> ToPaged<TSource>(this IQueryable<TSource> source, int page, int pageSize, out int rowsCount)
         {
             rowsCount = source.Count();
             return source.Skip((page - 1) * pageSize).Take(pageSize);
