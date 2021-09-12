ecefd687026841ae154c540decd6a62245fda158
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 23:32:11 +0430

    refactor : add get products for site service property

--- CleanArchitecture/CA.Application/Interfaces/FacadePatterns/IProductFacadePattern.cs
+++ CleanArchitecture/CA.Application/Interfaces/FacadePatterns/IProductFacadePattern.cs
@@ -2,12 +2,13 @@
 using CA.Application.Services.Product.Commands.AddNewProduct;
 using CA.Application.Services.Product.Queries.GetProductDetailsForAdmin;
 using CA.Application.Services.Product.Queries.GetProductsForAdmin;
+using CA.Application.Services.Product.Queries.GetProductsForSite;
 
 namespace CA.Application.Interfaces.FacadePatterns
 {
     public interface IProductFacadePattern
     {
-        public AddNewProductService AddNewProductService { get; }
+        public IAddNewProductService AddNewProductService { get; }
 
         /// <summary>
         /// دریافت لیست محصولات برای پنل مدیریت
@@ -15,5 +16,7 @@ namespace CA.Application.Interfaces.FacadePatterns
         public IGetProductsForAdminService GetProductsForAdminService { get; }
 
         public IGetProductDetailsForAdminService GetProductDetailsForAdminService { get; }
+
+        public IGetProductsForSiteService GetProductsForSiteService { get; }
     }
 }
