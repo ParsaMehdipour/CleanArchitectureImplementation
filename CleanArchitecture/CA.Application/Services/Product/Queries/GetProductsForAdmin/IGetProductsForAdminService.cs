aae04992d4ab0a6637a6a61c2427a4b1d759aeed
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:35:16 +0430

    feat : add i get products for admin service class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductsForAdmin/IGetProductsForAdminService.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Product.Queries.GetProductsForAdmin
+{
+    public interface IGetProductsForAdminService
+    {
+        ResultDto<ResultProductsForAdminDto> Execute(int Page = 1, int PageSize = 20);
+    }
+}
