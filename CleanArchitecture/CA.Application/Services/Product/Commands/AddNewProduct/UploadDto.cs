b0516d50120f206c9b66d94d1523160854fd1069
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:21:03 +0430

    feat : add upload dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Commands/AddNewProduct/UploadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CA.Application.Services.Product.Commands.AddNewProduct
+{
+    public class UploadResultDto
+    {
+        public long Id { get; set; }
+        public bool Status { get; set; }
+        public string FileNameAddress { get; set; }
+    }
+}
