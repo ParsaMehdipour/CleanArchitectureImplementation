ac3af61f744285dc08ff8f4973a303d8ba0041da
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 19:34:36 +0430

    feat : add product details images dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductDetailsForAdmin/ProductDetailsImagesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CA.Application.Services.Product.Queries.GetProductDetailsForAdmin
+{
+    public class ProductDetailsImagesDto
+    {
+        public long Id { get; set; }
+        public string Src { get; set; }
+    }
+}
