e77a9229fc4cbe0d780bc5b67661e030475df5c3
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:24:58 +0430

    feat : add products controller class

---
+++ CleanArchitecture/EndPoint.Site/Areas/AdminPanel/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using CA.Application.Interfaces.FacadePatterns;
+using CA.Application.Services.Product.Commands.AddNewProduct;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EndPoint.Site.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class ProductsController : Controller
+    {
+
+        private readonly IProductFacadePattern _productFacade;
+        private readonly ICategoryFacadePattern _categoryFacade;
+
+        public ProductsController(IProductFacadePattern productFacade,ICategoryFacadePattern categoryFacade)
+        {
+            _productFacade = productFacade;
+            _categoryFacade = categoryFacade;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddNewProduct()
+        {
+            ViewBag.Categories = new SelectList(_categoryFacade.GetAllCategoriesService.Execute().Data,"Id" , "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewProduct(RequestAddNewProductDto request, List<AddNewProductFeaturesDto> Features)
+        {
+            List<IFormFile> images = new List<IFormFile>();
+            foreach (var file in Request.Form.Files)
+            {
+                images.Add(file);
+            }
+            request.Images = images;
+            request.Features = Features;
+            return Json(_productFacade.AddNewProductService.Execute(request));
+        }
+    }
+}
