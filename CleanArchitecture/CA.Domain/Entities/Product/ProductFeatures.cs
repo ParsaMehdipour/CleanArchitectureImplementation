796bd1c8f44da27a95a7ba58d4e07600802f5b0f
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:22:35 +0430

    feat : add product feature domain class

---
+++ CleanArchitecture/CA.Domain/Entities/Product/ProductFeatures.cs
@@ -0,0 +1,12 @@
+﻿using CA.Domain.Entities.Common;
+
+namespace CA.Domain.Entities.Product
+{
+    public class ProductFeatures : BaseEntity
+    {
+        public virtual Product Product { get; set; }
+        public long ProductId { get; set; }
+        public string DisplayName { get; set; }
+        public string Value { get; set; }
+    }
+}
