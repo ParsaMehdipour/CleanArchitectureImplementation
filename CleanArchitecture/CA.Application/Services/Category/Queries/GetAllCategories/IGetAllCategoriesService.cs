2d90f3b6118ffc30a2bc201ae22b57c187d45562
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 16:12:18 +0430

    feat : add i get all categories service interface

---
+++ CleanArchitecture/CA.Application/Services/Category/Queries/GetAllCategories/IGetAllCategoriesService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CA.Common.Dto;
+
+namespace CA.Application.Services.Category.Queries.GetAllCategories
+{
+    public interface IGetAllCategoriesService
+    {
+        ResultDto<List<AllCategoriesDto>> Execute();
+    }
+}
