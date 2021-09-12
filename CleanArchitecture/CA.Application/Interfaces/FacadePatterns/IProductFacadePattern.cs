172d0e26b24fe15646896d9c8c43156b62e841e3
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-12 15:59:12 +0430

    feat : add i product facade pattern interface

---
+++ CleanArchitecture/CA.Application/Interfaces/FacadePatterns/IProductFacadePattern.cs
@@ -0,0 +1,10 @@
+﻿using CA.Application.Services.Category.Commands.AddNewCategory;
+using CA.Application.Services.Product.Commands.AddNewProduct;
+
+namespace CA.Application.Interfaces.FacadePatterns
+{
+    public interface IProductFacadePattern
+    {
+        public AddNewProductService AddNewProductService { get;}
+    }
+}
