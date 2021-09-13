973e543230f9a7aff011a1fd90b2883ff2fd64b4
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 11:19:53 +0430

    feat : add get menu item service class

---
+++ CleanArchitecture/CA.Application/Services/Common/Queries/GetMenuItem/GetMenuItemService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CA.Application.Interfaces.Contexts;
+using CA.Common.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA.Application.Services.Common.Queries.GetMenuItem
+{
+    public class GetMenuItemService : IGetMenuItemService
+    {
+        private readonly IDataBaseContext _context;
+        public GetMenuItemService(IDataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public ResultDto<ResultMenuItemDto> Execute()
+        {
+            var category = _context.Categories
+                .Where(p=> p.ParentCategoryId == null)
+                .Select(p => new MenuItemDto
+                {
+                    CatId = p.Id,
+                    Name = p.Name,
+                    Child = p.SubCategories.Select(child => new MenuItemDto
+                    {
+                        CatId = child.Id,
+                        Name = child.Name,
+                    }).ToList(),
+                }).ToList();
+
+            return new ResultDto<ResultMenuItemDto>()
+            {
+                Data = new ResultMenuItemDto()
+                {
+                    MenuItemDtos = category
+                },
+                IsSuccess = true,
+            };
+        }
+    }
+}
