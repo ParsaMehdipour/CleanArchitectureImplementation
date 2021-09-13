65e0530644dcaffdf5565c3045328aa676ecc93c
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 11:20:40 +0430

    feat : add result menu item dto class

---
+++ CleanArchitecture/CA.Application/Services/Common/Queries/GetMenuItem/ResultMenuItemDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Common.Queries.GetMenuItem
+{
+    public class ResultMenuItemDto
+    {
+        public List<MenuItemDto> MenuItemDtos { get; set; }
+    }
+}
