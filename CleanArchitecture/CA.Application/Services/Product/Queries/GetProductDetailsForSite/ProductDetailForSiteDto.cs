6c9a72b3bfa2bbdb2e4dd1e38b745fad18c3e41e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 14:25:10 +0430

    refactor : add get product details for site dto class

---
+++ CleanArchitecture/CA.Application/Services/Product/Queries/GetProductDetailsForSite/ProductDetailForSiteDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Product.Queries.GetProductDetailsForSite
+{
+    public class ProductDetailsForSiteDto
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public List<string> Images { get; set; }
+        public List<ProductDetailsForSiteFeaturesDto> Features { get; set; }
+    }
+}
