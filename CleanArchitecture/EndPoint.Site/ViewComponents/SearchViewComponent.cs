33159606022be03833d79e1cb27486ba84106b66
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 18:09:56 +0430

    feat : add search view component class

---
+++ CleanArchitecture/EndPoint.Site/ViewComponents/SearchViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using CA.Application.Services.Category.Queries.GetCategories;
+using CA.Application.Services.Common.Queries.GetCategoriesForSearch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.ViewComponents
+{
+    public class SearchViewComponent : ViewComponent
+    {
+        private readonly IGetCategoriesForSearchService _getCategoriesForSearchService;
+
+        public SearchViewComponent(IGetCategoriesForSearchService getCategoriesForSearchService)
+        {
+            _getCategoriesForSearchService = getCategoriesForSearchService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_getCategoriesForSearchService.Execute().Data.CategoryDtos);
+        }
+    }
+}
