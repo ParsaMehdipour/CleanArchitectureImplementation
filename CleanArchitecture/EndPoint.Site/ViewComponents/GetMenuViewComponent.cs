787752be308051938b173832463f3d050db7ee14
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 11:23:21 +0430

    feat : add get menu view component class

---
+++ CleanArchitecture/EndPoint.Site/ViewComponents/GetMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CA.Application.Services.Common.Queries.GetMenuItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Site.ViewComponents
+{
+    public class GetMenuViewComponent : ViewComponent
+    {
+        private readonly IGetMenuItemService _getMenuItemService;
+
+        public GetMenuViewComponent(IGetMenuItemService getMenuItemService)
+        {
+            _getMenuItemService = getMenuItemService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_getMenuItemService.Execute().Data.MenuItemDtos);
+        }
+    }
+}
