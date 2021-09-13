6ddb32b90a05b4975d3eeb7499c2db684b9120b3
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 11:20:22 +0430

    feat : add menu item dto class

---
+++ CleanArchitecture/CA.Application/Services/Common/Queries/GetMenuItem/MenuItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CA.Application.Services.Common.Queries.GetMenuItem
+{
+    public class MenuItemDto
+    {
+        public long CatId { get; set; }
+        public string Name { get; set; }
+        public List<MenuItemDto> Child { get; set; }
+    }
+}
