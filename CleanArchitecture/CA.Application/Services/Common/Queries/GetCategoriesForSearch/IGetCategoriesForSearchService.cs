1d90ddf8fae96ddede86d992135200bb9bf8be09
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-13 18:08:06 +0430

    feat : add i get categories for search service interface

---
+++ CleanArchitecture/CA.Application/Services/Common/Queries/GetCategoriesForSearch/IGetCategoriesForSearchService.cs
@@ -0,0 +1,9 @@
+﻿using CA.Common.Dto;
+
+namespace CA.Application.Services.Common.Queries.GetCategoriesForSearch
+{
+    public interface IGetCategoriesForSearchService
+    {
+        ResultDto<ResultGetCategoriesForSearch> Execute();
+    }
+}
